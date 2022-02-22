c34110de21d572b2b044f56716e294a6c45c21af
Tiffany Bilgore <tiff@tiff.tech>
2022-02-22 13:28:20 -0500

    Removed pathlib.Path completely and tested thigns still work

--- test.py
+++ test.py
@@ -7,20 +7,17 @@ import config
 import os
 import lbry
 import json
+import youtube
 
 def json_print(value):
     print(json.dumps(value, sort_keys=True, indent=4))
 
-settings = config.Settings(os.path.join(os.getcwd(),"test2"))
+settings = config.Settings(os.path.join(os.getcwd(),"test"))
+
+yt_channel = youtube.Channel(settings=settings, init_vids=False)
+lbry_channel = lbry.Channel(settings=settings, claim_id='8be45e4ba05bd6961619489f6408a0dc62f4e650', init_vids=False)
 
 
-channel = lbry.Channel(settings=settings, claim_id='8be45e4ba05bd6961619489f6408a0dc62f4e650', init_vids=False)
-
-video = lbry.Video(channel=channel,claim_id="1d1e00753d9998505b2fb2542827e10823cc8ef3")
+video = lbry.Video(channel=lbry_channel,claim_id="1d1e00753d9998505b2fb2542827e10823cc8ef3")
 
 video.update_local()
-
-video.title = "LBRY Upload Test"
-video.description = "Test upload and update with content creator manager dev project"
-
-video.update_lbry()
