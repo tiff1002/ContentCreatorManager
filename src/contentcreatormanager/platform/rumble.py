5b04a88618f504ad38a4265acf947092b07ad1d9
Tiffany Bilgore <tiff@tiff.tech>
2022-02-27 14:06:49 -0500

    Working on the Rumble Platform Class

--- src/contentcreatormanager/platform/rumble.py
+++ src/contentcreatormanager/platform/rumble.py
@@ -3,16 +3,42 @@ Created on Feb 24, 2022
 
 @author: tiff
 '''
-from contentcreatormanager.platform.platform import Platform
+import contentcreatormanager.platform.platform
+import os
+import json
 
-class Rumble(Platform):
+class Rumble(contentcreatormanager.platform.platform.Platform):
     '''
     classdocs
     '''
+    CLIENT_SECRETS_FILE = 'rumble_client_secret.json'
 
-
-    def __init__(self, params):
+    def __init__(self, settings : contentcreatormanager.config.Settings, init_videos : bool = False):
         '''
         Constructor
         '''
+        super(Rumble, self).__init__(settings=settings, ID='')
+        self.logger = self.settings.Rumble_logger
         
+        self.logger.info("Initializing Platform Object as a Rumble Platform Object")
+        
+        #change to the directory with the cred files
+        os.chdir(self.settings.original_dir)
+
+        #opent the rumble cred file and read with json
+        with open(Rumble.CLIENT_SECRETS_FILE) as f:
+            creds = json.load(f)
+            
+        #close file and move back to proper directory
+        f.close()
+        os.chdir(self.settings.folder_location)
+        
+        #set the access token and id from the creds file
+        self.access_token = creds['ACCESS_TOKEN']
+        self.id = creds['CHANNEL_ID']
+        
+        if init_videos:
+            #init vids
+            self.logger.info("Grabbing video data from Rumble")
+        
+        self.logger.info("Rumble Platform Object initialized")
--- src/test.py
+++ src/test.py
@@ -4,7 +4,7 @@ Created on Feb 24, 2022
 @author: tiff
 '''
 import os.path
-import contentcreatormanager.platform.youtube
+import contentcreatormanager.platform.rumble
 
 
 logging_config_file = os.path.join(os.getcwd(), "logging.ini")
@@ -13,9 +13,4 @@ test_folder = os.path.join(os.getcwd(), "test")
 settings = contentcreatormanager.config.Settings(logging_config_file=logging_config_file, folder_location=test_folder)
 
 
-yt = contentcreatormanager.platform.youtube.YouTube(settings=settings, init_videos=True)
-
-for v in yt.media_objects:
-    print(v.title)
-    
-print(f"There are {len(yt.media_objects)} uploads to this YouTube")
+rumble = contentcreatormanager.platform.rumble.Rumble(settings=settings)
